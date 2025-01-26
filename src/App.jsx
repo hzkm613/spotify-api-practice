782ba0523fb1a2eb5650c9da72928fbab678b4af
hzkm613 <hzkm613@gmail.com>
2025-01-26 18:30:27 -0500

    Set up Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <span>nav</span>
+    </div>
+  );
+};
+
+export default Navbar;
