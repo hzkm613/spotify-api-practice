041d3140c483cc3b8d7b275e550477a51751d379
hzkm613 <hzkm613@gmail.com>
2025-01-27 13:02:21 -0500

    Set up layouts of music player in Home

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,8 +2,25 @@ import React from "react";
 
 const Player = () => {
   return (
-    <div className="w-full h-full flex bg-amber-600">
-      <h1>Player</h1>
+    <div className="w-full h-full flex flex-col bg-amber-600">
+      <div className="w-full h-1/2 bg-amber-200">music player image</div>
+      <div className="w-full h-1/2 bg-blue-800">
+        <div className="h-10">
+          <span>Title</span>
+          <span>Not decided yet</span>
+        </div>
+        <div className="flex w-full bg-amber-400">
+          <span>1:53</span>
+          <div className="w-full bg-amber-300">bar</div>
+          <span>3:20</span>
+        </div>
+        <div className="w-full flex bg-amber-700">
+          <div>prev</div>
+          <button>Play button</button>
+          <div>next</div>
+          <div>repeat</div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/RobotDance.jsx
+++ src/components/RobotDance.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Dance from "../assets/image/dance.gif";
 
 const RobotDance = () => {
   return (
-    <div className="w-full h-full bg-amber-500">
-      <h1>Robot</h1>
+    <div className="w-full h-full flex justify-center items-center overflow-hidden bg-amber-500">
+      <div className="w-full h-full flex justify-center items-center">
+        <img
+          className="w-2/3 object-contain"
+          src={Dance}
+          alt="A gif of dancing Disney characters"
+        />
+      </div>
     </div>
   );
 };
