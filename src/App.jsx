536cf42b36f6aa9604b89c5974d801b156622c05
hzkm613 <hzkm613@gmail.com>
2025-01-26 19:07:59 -0500

    Set up Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Wrap with BrowserRouter
 import Home from "./pages/Home";
+import About from "./pages/About";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <div className="flex h-screen">
+        <Navbar />
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/image/logo.png";
+import SmartToyIcon from "@mui/icons-material/SmartToy";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
 
 const Navbar = () => {
   return (
-    <div>
-      <span>nav</span>
+    <div className="flex flex-col items-center w-1/2 h-full bg-gray-800 text-white p-4">
+      <div className="flex justify-center mb-4">
+        <img
+          src={Logo}
+          alt="Dr.Gabba Logo"
+          className="w-24 h-24 object-contain"
+        />
+      </div>
+      <ul className="space-y-8">
+        <li>
+          <Link to={"/"}>
+            <MusicNoteIcon />
+          </Link>
+        </li>
+        <li>
+          <Link to={"/about"}>
+            <SmartToyIcon />
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
